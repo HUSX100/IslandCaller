e88d66a3cfc10bbb5da373de44c2d95e9b364f08
宸睿 胡 <2908973918@qq.com>
2025-11-23 01:13:14 +0800

    初步迁移

---
+++ IslandCaller.App/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using IslandCaller.App.Models;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IslandCaller.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        Log.WriteLog("App.xaml.cs", "Debug", "IslandCaller Plugin Initializing");
+    }
+
+}
